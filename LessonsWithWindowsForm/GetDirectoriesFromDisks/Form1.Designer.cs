181cec69dd2da3ae127f121f2137961280673dfc
gabilgg07 <gabilgg07@gmail.com>
2022-05-10 12:22:46 +0400

    Lesson 112 ended: Anonymous, Callback, Lambda methods: from Windows

---
+++ LessonsWithWindowsForm/GetDirectoriesFromDisks/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace GetDirectoriesFromDisks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFromC = new System.Windows.Forms.Button();
+            this.btnFromD = new System.Windows.Forms.Button();
+            this.lstDirs = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnFromC
+            // 
+            this.btnFromC.Location = new System.Drawing.Point(42, 40);
+            this.btnFromC.Name = "btnFromC";
+            this.btnFromC.Size = new System.Drawing.Size(205, 88);
+            this.btnFromC.TabIndex = 0;
+            this.btnFromC.Text = "From C:";
+            this.btnFromC.UseVisualStyleBackColor = true;
+            this.btnFromC.Click += new System.EventHandler(this.btnFromC_Click);
+            // 
+            // btnFromD
+            // 
+            this.btnFromD.Location = new System.Drawing.Point(42, 324);
+            this.btnFromD.Name = "btnFromD";
+            this.btnFromD.Size = new System.Drawing.Size(205, 88);
+            this.btnFromD.TabIndex = 1;
+            this.btnFromD.Text = "From D:";
+            this.btnFromD.UseVisualStyleBackColor = true;
+            this.btnFromD.Click += new System.EventHandler(this.btnFromD_Click);
+            // 
+            // lstDirs
+            // 
+            this.lstDirs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstDirs.FormattingEnabled = true;
+            this.lstDirs.ItemHeight = 16;
+            this.lstDirs.Location = new System.Drawing.Point(253, 40);
+            this.lstDirs.Name = "lstDirs";
+            this.lstDirs.Size = new System.Drawing.Size(541, 372);
+            this.lstDirs.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstDirs);
+            this.Controls.Add(this.btnFromD);
+            this.Controls.Add(this.btnFromC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFromC;
+        private System.Windows.Forms.Button btnFromD;
+        private System.Windows.Forms.ListBox lstDirs;
+    }
+}
+
---
+++ LessonsWithWindowsForm/GetDirectoriesFromDisks/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetDirectoriesFromDisks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFromC_Click(object sender, EventArgs e)
+        {
+            // burda yazdigimiz kodlar ancaq bu buttonda istifade edildiyi ucun
+            // ayrica method yazmagimiza ehtiyac yoxdur.
+            // Anonymous methodla RunSafe delegatemize oture bilerik:
+            RunSafe(delegate {
+                string[] dirs = Directory.GetDirectories("C:\\");
+
+                lstDirs.Items.Clear();
+
+                foreach (var item in dirs)
+                {
+                    lstDirs.Items.Add(item);
+                }
+            });
+
+            // Anonymous methodlarin parametr qebul eden formada:
+
+            Action<string> tAction = delegate (string str)
+            {
+                MessageBox.Show($"Anonymous method's parametr value is: {str}");
+            };
+
+            tAction("AnonymousMethodSValue");
+        }
+
+        private void btnFromD_Click(object sender, EventArgs e)
+        {
+            // Lambda methods:
+            // => isaresi Lambda isaresi adlanir.
+            // if-deki basqadi.
+            // () methodun bodisi {} bu olsun deyende Lambda isaresi sayilir.
+            // Asp.Net de cox istifade edilecek.
+            RunSafe(() => {
+                string[] dirs = Directory.GetDirectories("D:\\");
+
+                lstDirs.Items.Clear();
+
+                foreach (var item in dirs)
+                {
+                    lstDirs.Items.Add(item);
+                }
+            });
+
+            // Lambda methodlarin parametr qebul eden formasi:
+            Action<int> tAction = (int x) =>
+            {
+                MessageBox.Show($"Lambda method's parametr value is: {x}");
+            };
+
+            tAction(11);
+        }
+
+        // Bele bir method yazirik ki, tehlukesis button clicklerimizi islede bilek.
+        // Nese problem olanda partlamasin, mesaj kimi mene gostersin.
+
+        // callback methods adlanir.
+        // yeni argument kimi gonderilmis method (bodyde) cagrilir,
+        // harda gonderilibse orada isleyir.
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action">Is callback method</param>
+        void RunSafe(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Xeta", 0, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
